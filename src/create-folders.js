92731d5dd60f0cf4371040c64e86fa3febfa74c5
seth-js <83692925+seth-js@users.noreply.github.com>
2023-10-22 04:00:32 -0500

    Removed outdated code

--- src/create-folders.js
+++
@@ -1,7 +0,0 @@
-const { mkdirSync } = require('fs');
-
-const folders = ['freq', 'kaikki', 'sentences', 'tidy', 'yomichan'];
-
-for (const folder of folders) {
-  mkdirSync(`data/${folder}`, { recursive: true });
-}
